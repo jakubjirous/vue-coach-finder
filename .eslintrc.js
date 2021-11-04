f5bece0d5947e53dd425437aac603bbcde968aa6
Jakub Jirous <info@jakubjirous.cz>
2021-11-04 09:37:02 -0700

    [config] vue project setup

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-quotes': ['error', 'double', { avoidEscape: false }],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never',
+      },
+    ],
+    indent: ['error', 2],
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2],
+    'vue/multiline-html-element-content-newline': 0,
+    quotes: ['error', 'single', { avoidEscape: false }],
+  },
+};
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Vue Coach Finder
+# vue-coach-finder
 
-Vue.js - Routing, Vuex, Components
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
